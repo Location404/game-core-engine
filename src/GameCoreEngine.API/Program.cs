812f437852592d5e794f54e8396e31aab539acfc
ryanbromati <ryan.bromati@hotmail.com>
2025-10-17 11:08:28 -0300

    feat: adicionar configuração de CORS e suporte a SignalR no aplicativo

--- src/GameCoreEngine.API/Program.cs
+++ src/GameCoreEngine.API/Program.cs
@@ -1,9 +1,38 @@
+using GameCoreEngine.API.Hubs;
+using GameCoreEngine.Infrastructure.Extensions;
+
+using Shared.Observability.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddSignalR();
+
+builder.Services.AddOpenTelemetryObservability(builder.Configuration, options =>
+{
+    options.Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+                builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() 
+                ?? ["http://localhost:4200"]
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 app.MapOpenApi();
+app.MapHub<GameHub>("/gamehub");
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
 app.Run();
